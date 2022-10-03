27d5f20d75c3453413b4236183092eecc1de480c
misaiii <aileen_vicente@dlsu.edu.ph>
2022-10-03 09:35:06 +0800

    barebones running

--- app.py
+++ app.py
@@ -1,30 +1,50 @@
-from flask import Flask, render_template, request
 import json
+import re
+import random
+
 
 data_file = open('data.json').read()
-intents = json.loads(data_file)
+intents_json = json.loads(data_file)
+rules_dict = {1:re.compile("[A-Za-z\s?!\.]*[H|hello][A-Za-z\s?!\.]")}
 
+def getTag(msg): 
+    tag = re.sub(r"([A-Za-z\s?!\.]*[H|hello][A-Za-z\s?!\.])","greeting", msg)
+    #tag = "greeting"
+    #rules_list=rules_dict['patterns']
+    #for j in rules_list:
+    #    if re.search(j['regex'],msg):
+    #        tag = j['tag']
+    #        break
+    
+    return tag
 
 def getResponse(msg):
-    """ tag = ints[0]['intent']
+    tag = getTag(msg)
+    result = msg
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if(i['tag']== tag):
             result = random.choice(i['responses'])
-            break """
-    return msg
+            break
+    return result
 
 def chatbot_response(msg):
     res = getResponse(msg)
-    return intents
+    return res
 
+from flask import Flask, render_template, request
 app = Flask(__name__)
+#app.static_folder = 'static'
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route("/getResponse")
 def get_bot_response():
     userText = request.args.get('msg')
     return chatbot_response(userText)
+
+#if __name__ == "__main__":
+#    app.run()
