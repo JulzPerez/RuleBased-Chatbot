b672b8f51ba052da6bf3fe490f073046ae77c61d
misaiii <aileen_vicente@dlsu.edu.ph>
2022-10-03 11:25:44 +0800

    reads rules from text file, not tested thoroughly

--- app.py
+++ app.py
@@ -5,17 +5,29 @@ import random
 
 data_file = open('data.json').read()
 intents_json = json.loads(data_file)
-rules_dict = {1:re.compile("[A-Za-z\s?!\.]*[H|hello][A-Za-z\s?!\.]")}
+#rules_dict = {1:re.compile("[A-Za-z\s?!\.]*[H|hello][A-Za-z\s?!\.]")}
+
+def loadRules():
+    myFile = open('rules.txt','r')
+    rLine = myFile.readlines()
+    rules_dict = {}
+    x=1
+    for line in rLine:
+        regex, tag = line.split()
+
+        rules_dict[x] = {'regex': regex, 'tag': tag}
+        x=x+1
+
+    return (rules_dict) 
 
 def getTag(msg): 
-    tag = re.sub(r"([A-Za-z\s?!\.]*[H|hello][A-Za-z\s?!\.])","greeting", msg)
-    #tag = "greeting"
-    #rules_list=rules_dict['patterns']
-    #for j in rules_list:
-    #    if re.search(j['regex'],msg):
-    #        tag = j['tag']
-    #        break
-    
+    rules = loadRules()
+    i=1
+    for i in rules.keys():
+        if re.match(rules[i]['regex'],msg):
+            tag=(rules[i]['tag'])
+            break
+        
     return tag
 
 def getResponse(msg):
