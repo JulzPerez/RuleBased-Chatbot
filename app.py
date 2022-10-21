dcea7b85e1386619e79b1b3a8d1af5d9c1aa39fd
MIS Director <mis.director@uic.edu.ph>
2022-10-21 08:40:05 +0800

    modified some parts to accommodate conversation sequence

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import json
 import re
 import random
 import sys
+from flask import Flask,render_template,request,session
+
+app = Flask(__name__)
+# Set the secret key to some random bytes. Keep this really secret!
+app.config.update(SECRET_KEY='osd(99092=36&462134kjKDhuIS_d23',
+                  ENV='development')
 
 data_file = open('data.json').read()
 intents_json = json.loads(data_file)
+current_client=""
+current_msg_level=""
 
 #read regex rules from files
 def loadRules():
@@ -12,26 +20,47 @@ def loadRules():
     rLine = myFile.readlines()
     rules_dict = {}
     x=1
-    for line in rLine:
-        regex, tag = line.split()
-        rules_dict[x] = {'regex': regex, 'tag': tag}
-        x=x+1
+    print(
+        "current_client: ",session["current_client"],
+        "\ncurrent_msg_level: ",session["current_msg_level"],
+        "\n",file=sys.stderr)
 
+    for line in rLine:
+        if line.strip()=="" or "#" in line: #skip blank lines and comments
+            continue
+
+        regex, tag = line.split()
+        
+        if current_msg_level=="generic":
+            if(("_first_" in tag and current_client in tag) or ("generic" in tag and current_client in tag)):
+                rules_dict[x] = {'regex': regex, 'tag': tag}
+            
+            elif("generic" in tag):
+                rules_dict[x] = {'regex': regex, 'tag': tag}
+        
+        elif current_msg_level=="first":
+            if(("_second_" in tag and current_client in tag)("generic" in tag and current_client in tag)):
+                rules_dict[x] = {'regex': regex, 'tag': tag}
+
+        elif current_msg_level=="second":
+            if(("_third_" in tag and current_client in tag)("generic" in tag and current_client in tag)):
+                rules_dict[x] = {'regex': regex, 'tag': tag}
+
+
+        x=x+1
 
     return (rules_dict) 
 
 #get corresponding tag from triggered regex rule
 def getTag(msg): 
     rules=loadRules() #will load Rules each time, optimize this 
+    print("rules: ",rules,file=sys.stderr)
     intent = "" #will return empty string if no rule matched
     for i in rules.keys():
         pattern = rules[i]['regex']
-        print(pattern,msg, file=sys.stderr)
-        
+
         if(re.search(pattern,msg)):
             intent = rules[i]['tag']
-            print("found!",pattern, file=sys.stderr)
-            print("intent: ",intent, file=sys.stderr)
             break
         
     return intent
@@ -39,6 +68,7 @@ def getTag(msg):
 #get response based on intent or tag (i.e. tag associated with user message)
 def getResponse(msg):
     tag = getTag(msg)
+    print("tag: ",tag,"\nmessage:",msg,file=sys.stderr)
     result = msg
     list_of_intents = intents_json['intents']
     if (tag == ""): #no rule matched, hence ask for clarification
@@ -58,19 +88,30 @@ def chatbot_response(msg):
     res = getResponse(msg)
     return res
 
-from flask import Flask, render_template, request
-app = Flask(__name__)
-#app.static_folder = 'static'
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/getResponse")
 def get_bot_response():
-    userText = request.args.get('msg')    
+    userText = request.args.get('msg')        
     return chatbot_response(userText.lower())
 
-#if __name__ == "__main__":
-#    app.run()
+@app.route("/setClient")
+def set_client():
+    if session.get('current_client'):
+        if session['current_client']!="":
+            return "already set"
+
+    session['current_client'] = request.args.get('client')
+    session['current_msg_level']='generic'
+    
+    return "ok"
+
+@app.route("/clearSessions")
+def clear_sessions():
+    # session.pop('current_client', None)
+    # session.pop('current_msg_level', None)
+    session['current_client']=''
+    session['current_msg_level'] = 'generic'
+    return "ok"
